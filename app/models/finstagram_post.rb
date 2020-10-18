cca2b538cd92d513fff3d11e4287caf13ac8bb72
wolokun <wolokun@gmail.com>
2020-10-18 22:29:27 +0000

    adds models and signup handling GET and POST

---
+++ app/models/finstagram_post.rb
@@ -0,0 +1,30 @@
+class FinstagramPost < ActiveRecord::Base
+
+    belongs_to :user
+    has_many :comments
+    has_many :likes
+
+    validates_presence_of :user
+
+    def humanized_time_ago
+        time_ago_in_minutes = Time.now - self.created_at
+        time_ago_in_minutes = time_ago_in_minutes / 60
+
+        if time_ago_in_minutes >= 60
+            "#{(time_ago_in_minutes / 60) .to_i} hours ago"
+        else
+            "#{time_ago_in_minutes.to_i} minutes ago"
+        end
+    end
+    # New Stuff Start
+    def like_count
+        self.likes.size
+    end
+
+    def comment_count
+        self.comments.size
+    end
+
+    # New Stuff End
+
+end
